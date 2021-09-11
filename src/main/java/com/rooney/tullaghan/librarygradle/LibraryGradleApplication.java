7fcd7e86c29411d05d200c70f2c9ad37c1a68e1d
jamesvrooney <jamesvrooney@hotmail.com>
2021-09-11 05:30:29 +0100

    Initial commit.

---
+++ src/main/java/com/rooney/tullaghan/librarygradle/LibraryGradleApplication.java
@@ -0,0 +1,13 @@
+package com.rooney.tullaghan.librarygradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rooney/tullaghan/librarygradle/LibraryGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rooney.tullaghan.librarygradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
